ce2602be05ab06edbcbbae608210a4bd129409c8
Yurim <152935798+zyu22@users.noreply.github.com>
2024-08-04 22:14:26 +0900

    [BOJ] 그대로 출력하기 / 브론즈 2 / 5분
    https://www.acmicpc.net/problem/11718

---
+++ 지유림/baekjoon/11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+			
+		while(sc.hasNext()) {
+			String s = sc.nextLine();
+			System.out.println(s);
+		}
+		
+	}
+}
